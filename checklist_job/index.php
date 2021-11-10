ba6fddbaf55f12ef2254bc427085103f63f1252a
Dicky Erfan Septiono <62490370+dicky495@users.noreply.github.com>
2021-11-10 21:19:10 +0700

    Add files via upload

--- checklist_job/index.php
+++ checklist_job/index.php
@@ -139,18 +139,14 @@ if (isset($_GET['delete'])) {
                                 <li class="list-group-item">
                                     <?php echo $name_task; ?>
                                     <div style="float: right;">
-                                        <a href="index.php?edit=<?php echo $id_task ?>">
-                                            <span class="badge bg-danger" id="proses">Proses</span>
-                                            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
-                                                <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z" />
-                                                <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
-                                            </svg> -->
+                                        <a href="index.php?delete=<?php echo $id_task ?>">
+                                            <span class="badge bg-danger" id="proses">Hapus</span>
                                         </a>
-                                        <!-- <a href="index.php?delete=<?php echo $id_task ?>" class="btn btn-danger">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                                            </svg>
-                                        </a> -->
+                                    </div>
+                                    <div style="float: right;">
+                                        <a href="index.php?edit=<?php echo $id_task ?>">
+                                            <span class="coba badge bg-danger" id="proses">Proses</span>
+                                        </a>
                                     </div>
                                 </li>
                             <?php } ?>
@@ -225,11 +221,18 @@ if (isset($_GET['delete'])) {
             })
         });
 
-        // const proses = document.getElementsByClassName('list-group-item');
-        // console.info(proses);
-        // proses.addEventListener('mouseover', function() {
-        //     proses.innerHTML = "Pekerjaan telah Selesai";
-        // });
+        const proses = document.querySelectorAll('.coba');
+        for (let i = 0; i < proses.length; i++) {
+            proses[i].addEventListener('mouseover', function() {
+                proses[i].innerHTML = "Pekerjaan telah selesai";
+            });
+            proses[i].addEventListener('mouseleave', function() {
+                proses[i].innerHTML = "Proses";
+            });
+            proses[i].addEventListener('click', function() {
+                proses[i].style.display = "none";
+            });
+        }
     </script>
 
 
